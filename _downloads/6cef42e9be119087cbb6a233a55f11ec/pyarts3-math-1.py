06f77c6c759fd56a383240ae6918c0161fa8f327
Oliver Lemke <olemke@gmail.com>
2025-09-16 09:00:05 +0200

    Update docs

--- _downloads/6cef42e9be119087cbb6a233a55f11ec/pyarts3-math-1.py
+++ _downloads/6cef42e9be119087cbb6a233a55f11ec/pyarts3-math-1.py
@@ -6,7 +6,7 @@ x = [0.1, 0.4, 0.6, 0.9]
 y = np.arcsin(x)
 
 xn = 0.5
-yn = pyarts.math.interp(y, pyarts.arts.interp.LagrangeCyclic(xn, x, 1))
+yn = pyarts.math.interp(y, pyarts.arts.interp.LagrangeCyclic(x, xn, 1))
 
 plt.plot(x, y, '-x')
 plt.plot([xn], [yn], '*')
